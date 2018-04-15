d516882c5e12968d8127d75578baab540a5168c7
Larry Hernandez <larryhernandez@users.noreply.github.com>
2018-04-15 19:02:44 -0400

    fix: update on_epoch_end() to make predictions

--- capstone/auxiliary_functions/project_metrics.py
+++ capstone/auxiliary_functions/project_metrics.py
@@ -4,8 +4,10 @@ Created on Tue Mar  6 20:51:10 2018
 
 @author: Larry
 """
+import numpy as np
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
 from keras.callbacks import Callback
+
 
 def microaveraged_f1score(y_true, y_pred):
     '''   '''
@@ -18,23 +20,26 @@ class Metrics(Callback):
     ''' 
     class that calculates f1, precision, and recall scores after 
     
-    Code borrowed from the following blog:
+    Code borrowed (and adapted) from the following blog:
     https://medium.com/@thongonary/how-to-compute-f1-score-for-each-epoch-in-keras-a1acd17715a2
     '''
     
     def on_train_begin(self, logs={}):
-     self.val_f1s = []
-     self.val_recalls = []
-     self.val_precisions = []
- 
+        self.val_f1s = []
+        self.val_recalls = []
+        self.val_precisions = []
+        return
+    
     def on_epoch_end(self, epoch, logs={}):
-     val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
-     val_targ = self.model.validation_data[1]
-     _val_f1 = f1_score(val_targ, val_predict)
-     _val_recall = recall_score(val_targ, val_predict)
-     _val_precision = precision_score(val_targ, val_predict)
-     self.val_f1s.append(_val_f1)
-     self.val_recalls.append(_val_recall)
-     self.val_precisions.append(_val_precision)
-     print("— val_f1: %f — val_precision: %f — val_recall %f" %(_val_f1, _val_precision, _val_recall))
-     return
+        val_p = self.model.predict(self.validation_data[0])
+        val_predict = [np.argmax(val_p[idx]) for idx in range(len(val_p))]
+        val_t = self.validation_data[1]
+        val_targ = [np.argmax(val_t[idx]) for idx in range(len(val_t))]
+        _val_f1 = f1_score(val_targ, val_predict, average = 'micro')
+        val_recall_score = recall_score(val_targ, val_predict, average = 'micro')
+        val_precision_score = precision_score(val_targ, val_predict, average = 'micro')
+        self.val_f1s.append(_val_f1)
+        self.val_recalls.append(val_recall_score)
+        self.val_precisions.append(val_precision_score)
+        print("— val_f1: {f1}".format(f1 = _val_f1))
+        return
